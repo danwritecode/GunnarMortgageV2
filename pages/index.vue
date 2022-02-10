36dee987da127c114abe833a71aaa3a6fd09dd5a
danwritecode <dnelson4993@outlook.com>
2022-02-09 22:56:43 -0500

    mobile formatting

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="min-h-[100vh] overflow-hidden flex items-center justify-center max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 lg:py-24">
     <div class="text-left w-full">
-      <h1 class="text-7xl 2xl:text-8xl font-bold max-w-4xl 2xl:max-w-6xl">Isn't it time you used Gunnar for your mortgage?</h1>
+      <h1 class="text-5xl sm:text-6xl lg:text-7xl 2xl:text-8xl font-bold lg:max-w-4xl 2xl:max-w-6xl">Isn't it time you used Gunnar for your mortgage?</h1>
       <div class="ml-2.5 flex justify-start">
-        <div class="mt-16 2xl:mt-20 relative bg-teal-400 w-96 2xl:w-[27rem] h-20">
-          <button class="w-96 2xl:w-[27rem] h-20 absolute -left-2 -top-2 hover:-left-2.5 hover:-top-2.5 px-6 py-4 uppercase font-semibold tracking-wider bg-black text-white text-2xl hover:bg-gray-900 transition-hover-300">
+        <div class="mt-16 2xl:mt-20 relative bg-teal-400 w-72 lg:w-96 2xl:w-[27rem] h-16 md:h-20">
+          <button class="w-72 lg:w-96 2xl:w-[27rem] h-16 md:h-20 absolute -left-2 -top-2 hover:-left-2.5 hover:-top-2.5 px-6 py-4 uppercase font-semibold tracking-wider bg-black text-white text-xl lg:text-2xl hover:bg-gray-900 transition-hover-300">
             Get Started Now
           </button>
         </div>  
       </div>
     </div>
-    <div class="absolute inset-y-0 right-0 flex items-center">
-      <img src="~/assets/img/gun_half_face.svg" alt="Gunnar Half Face" class="h-3/4">
+    <div class="absolute inset-y-0 top-7 right-7 lg:right-0 lg:top-0 top flex lg:items-center">
+      <img src="~/assets/img/gun_half_face.svg" alt="Gunnar Half Face" class="hidden lg:block lg:h-1/2 xl:h-2/3 2xl:h-3/4">
+      <img src="~/assets/img/logo.png" alt="" class="h-20 lg:hidden w-auto">
     </div>
     <div class="absolute inset-y-0 left-0 flex flex-col justify-between">
-      <img src="~/assets/img/triangle.svg" alt="triangle shape" class="h-40 w-auto 2xl:h-auto">
-      <img src="~/assets/img/circle.svg" alt="circle shape" class="h-32 w-auto 2xl:h-auto">
+      <img src="~/assets/img/triangle.svg" alt="triangle shape" class="h-40 w-auto lg:h-48 2xl:h-72">
+      <img src="~/assets/img/circle.svg" alt="circle shape" class="h-32 w-auto lg:h-32 2xl:h-64">
     </div>
   </div>
 </template>
