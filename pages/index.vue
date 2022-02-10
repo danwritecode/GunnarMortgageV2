f24ce544eadd0af7889bdd13014e2942c73ba272
danwritecode <dnelson4993@outlook.com>
2022-02-09 22:29:47 -0500

    updated mobile formatting, added more shapes to index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="min-h-[100vh] overflow-hidden flex items-center justify-center max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 lg:py-24">
     <div class="text-left w-full">
-      <h1 class="text-8xl font-bold max-w-6xl">Isn't it time you used Gunnar for your mortgage?</h1>
+      <h1 class="text-7xl 2xl:text-8xl font-bold max-w-4xl 2xl:max-w-6xl">Isn't it time you used Gunnar for your mortgage?</h1>
       <div class="ml-2.5 flex justify-start">
-        <div class="mt-20 relative bg-teal-400 w-[27rem] h-20">
-          <button class="w-[27rem] h-20 absolute -left-2 -top-2 hover:-left-2.5 hover:-top-2.5 px-6 py-4 uppercase font-semibold tracking-wider bg-black text-white text-2xl hover:bg-gray-900 transition-hover-300">
+        <div class="mt-16 2xl:mt-20 relative bg-teal-400 w-96 2xl:w-[27rem] h-20">
+          <button class="w-96 2xl:w-[27rem] h-20 absolute -left-2 -top-2 hover:-left-2.5 hover:-top-2.5 px-6 py-4 uppercase font-semibold tracking-wider bg-black text-white text-2xl hover:bg-gray-900 transition-hover-300">
             Get Started Now
           </button>
         </div>  
       </div>
     </div>
-    <div>
-      <img src="~/assets/img/gun_half_face.svg" alt="Gunnar Half Face" class="absolute inset-y-0 right-0 mt-20">
+    <div class="absolute inset-y-0 right-0 flex items-center">
+      <img src="~/assets/img/gun_half_face.svg" alt="Gunnar Half Face" class="h-3/4">
+    </div>
+    <div class="absolute inset-y-0 left-0 flex flex-col justify-between">
+      <img src="~/assets/img/triangle.svg" alt="triangle shape" class="h-40 w-auto 2xl:h-auto">
+      <img src="~/assets/img/circle.svg" alt="circle shape" class="h-32 w-auto 2xl:h-auto">
     </div>
   </div>
 </template>
